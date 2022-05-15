5c762d6d88f923603d970fc268a8849c1681b9a8
Carsten Wandel <carsten.wand@gmail.com>
2022-05-15 22:26:37 +0200

    Added String Hook and Reinforced String. Completed Net Rod.

--- src/main/java/net/parcival/fishingrevamp/item/ModItems.java
+++ src/main/java/net/parcival/fishingrevamp/item/ModItems.java
@@ -6,12 +6,14 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.parcival.fishingrevamp.FishingRevamp;
+import org.lwjgl.system.CallbackI;
 
 public class ModItems {
 
     public static final Item NET_ROD = registerItem("net_rod", new NetRodItem(new FabricItemSettings().group(ItemGroup.TOOLS).maxCount(1).maxDamage(64)));
     public static final Item FISHING_NET = registerItem("fishing_net", new Item(new FabricItemSettings().group(ItemGroup.MISC).maxCount(64)));
-
+    public static final Item REINFORCED_STRING = registerItem("reinforced_string", new Item(new FabricItemSettings().group(ItemGroup.MISC).maxCount(64)));
+    public static final Item STRING_HOOK = registerItem("string_hook", new Item(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
 
 
 
